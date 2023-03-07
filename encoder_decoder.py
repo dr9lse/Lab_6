a0d2304fab93fe30f2c21e442aa33eede46bf8b5
dr9lse <116731563+dr9lse@users.noreply.github.com>
2023-03-07 16:45:23 -0500

    sup

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -4,11 +4,4 @@ def encode(string):
         newstring += str((int(x) + 3) % 10)
     return newstring
 
-def decode(string):
-    newstring = ''
-    for x in string:
-        newstring += str((int(x) - 3) % 10)
-    return newstring
 
-
-print(decode('123'))
